891fd5d0740ea42fbe74d7e61702735685a32dcf
Spencer Williams <srwilliams349@gmail.com>
2023-03-20 00:41:01 -0400

    finish MakeChangeProject and README

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # MakeChangeProject
 
 ## Overview
-
+Create a cash register that determines the correct change based on inputs (price & amount tendered) and directs the user to return the largest bill and coin denominations possible.
 
 ## Technologies Used
 - Java
@@ -9,5 +9,15 @@
 - Git
 
 ## Lessons Learned
-... reflections of what we learned in this project. 
-... converting doubles to int and truncation etc.
+- First step was applying basic Java commands to print out instructions for the user, as well as importing a scanner to read the required inputs of price and amount tendered. The data type 'double' was used to capture inputs given the need for fractions of whole numbers when dealing with accurate change down to a penny.
+- The next step was to calculate the correct change and assign it a variable 'change', for which I again used a double. 
+- I then created an 'if statement' to determine if the amount tendered was below the purchase price. If true this would print out an error message.
+- Similarly, I used an 'else if' to determine if the exact change was tendered, which would close the program. For both statements the use of 'return;' made sure the program ended without continuing through the code and printing the final line "Provide customer change: ". 
+
+- The hardest part of this project for me was creating a way to calculate and show only the largest required denominations possible. I leaned towards using the logic from the Floor and Ceiling labs we completed on 3/17/23 to calculate the answer.
+- As part of this calculation I needed to cast the double values to int's for each denomination value and determine how may were included in the variable 'change'. 
+- Then I used the modulus operator in a simple formula ('change' %= [denomination]) to calculate the remainder. These last two steps were done for each denomination from $20 down to a penny.
+- To get the remainder of a penny to round properly I had to add +0.005 to the first formula which cast the user double to an int. 
+
+- The final step was creating a series of 'if statements' to print out the appropriate change if true. For this I placed them in descending order from largest to smallest denominations.
+
--- src/MakeChange.java
+++ src/MakeChange.java
@@ -9,11 +9,14 @@ public class MakeChange {
 //		Amount: .59, Tendered: 1.00, Result: 1 quarter, 1 dime, 1 nickel, 1 penny.
 //		Amount: 3.96, Tendered: 20.00, Result: 1 ten dollar bill, 1 five dollar bill, 1 one dollar bill, 4 pennies.
 //		Amount: any amount less than 20.00, Tendered: anything greater than amount: correct denominations for correct change.
-//		TO EDIT README OPEN WITH TEXT EDITOR
+// 		----------------------------------------------------------
+//		HINT: add 0.005 to truncate or use Math.round((num] / 100);
 //		/Users/spencercomputer/SD/Java/workspace/MakeChangeProject
-		
-		Scanner sc = new Scanner(System.in);
-		
+//		git add .
+//		git commit -m "[insert message]"
+//		git push [will push updates]
+
+		Scanner sc = new Scanner(System.in);	//Scanner to capture inputs(doubles)
 		System.out.println("How much is the item? ");
 		double cost = sc.nextDouble();
 		System.out.println("How much did the customer give you? ");
@@ -21,9 +24,64 @@ public class MakeChange {
 		
 		sc.close();
 		
+		double change = paid - cost;			//determine amount of change to give
+		
+		if (change < 0.0) {						//if too little change = error
+			System.out.println("Error! Customer did not pay enough.");
+			return;								//return(s) allow output to flow properly. For example stops the output from printing this line & "provide customer change" together. 
+		}
+		else if (change == 0.0) {				//if exact change = end
+			System.out.println("No change needed.");
+			return;
+		}
 		
 		
+		int twentyDollar = (int) (change / 20);	//cast doubles to ints. determine how many of variable is in change
+		change %= 20;							//mod for remainder
+//			System.out.println(twentyDollar);   //This shows how many $20 there are change :: change ( paid - cost)
+//			System.out.println(change);			//This shows how much change is left
+		int tenDollar = (int) (change / 10);
+		change %= 10;
+//			System.out.println(tenDollar); 		//shows # of $10 in change..
+//			System.out.println(change);			//... shows change remaining
+		int fiveDollar = (int) (change / 5);
+		change %= 5;
+		int oneDollar = (int) (change / 1);
+		change %= 1;
+		int quarter = (int) (change / 0.25);
+		change %= 0.25;
+		int dime = (int) (change / 0.10);
+		change %= 0.10;
+		int nickel = (int) (change / 0.05);
+		change %= 0.05;
+		int penny = (int) (change / 0.01 + 0.005);		//+0.005 to round properly
+		change %= 0.01;
 		
-	}
-
+		System.out.println("Provide customer change: ");
+		if (twentyDollar > 0 ) {				//if statements in descending order to print output from calculations above if true
+			System.out.println(twentyDollar + " twenty dollar bill(s) ");
+		}
+		if (tenDollar > 0 ) {
+			System.out.println(tenDollar + " ten dollar bill(s) ");
+		}
+		if (fiveDollar > 0 ) {
+			System.out.println(fiveDollar + " five dollar bill(s) ");
+		}
+		if (oneDollar > 0 ) {	
+			System.out.println(oneDollar + " one dollar bill(s) ");
+		}
+		if (quarter > 0 ) {
+			System.out.println(quarter + " quarter(s) ");
+		}
+		if (dime > 0 ) {
+			System.out.println(dime + " dime(s) ");
+		}
+		if (nickel > 0 ) {
+			System.out.println(nickel + " nickel(s) ");
+		}
+		if (penny > 0 ) {
+			System.out.println(penny + " penny(pennies) ");
+		}
+		
+	}	
 }
